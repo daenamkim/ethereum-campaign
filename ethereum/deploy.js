f3e2636937da9056e73f73e278dc75f3ea7a6e88
Daenam Kim <daenam.kim@gmail.com>
2019-04-03 10:58:03 +0900

    Add contract deploy

---
+++ ethereum/deploy.js
@@ -0,0 +1,41 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.RINKEBY_API
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
+// Addresses
+// 0x7B1C43b2C3F735Bbb230836aCe954f7551532D5a
+
+/*
+ truffle-hdwallet-provider version 0.0.3
+
+const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+
+truffle-hdwallet-provider versions 0.0.4, 0.0.5 and 0.0.6
+
+const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: '0x' + compiledFactory.bytecode }) // add bytecode
+    .send({ from: accounts[0] }); // remove gas
+
+ */
