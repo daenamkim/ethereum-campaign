092fe056b436bcdab387fe45988406512ffd4827
Daenam Kim <daenam.kim@gmail.com>
2019-04-07 20:39:44 +0900

    Better layout

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,14 @@ class CampaignIndex extends Component {
             rel="stylesheet"
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
+          <h3>Open Campaigns</h3>
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
           {this.renderCampaigns()}
-          <Button content="Create Campaign" icon="add circle" primary />
         </div>
       </Layout>
     );
