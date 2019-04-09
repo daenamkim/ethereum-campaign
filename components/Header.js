72a58d0bfc6bb9598d5d256975a234176466bf9b
Daenam Kim <daenam.kim@gmail.com>
2019-04-09 13:57:15 +0900

    Add router mappings

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-      <Menu.Item>CrowdCoin</Menu.Item>
+      <Link route="/">
+        {/* If you want to see a href in the html use a tag  */}
+        {/* <a className="item">CrowdCoin</a> */}
+        <Menu.Item>CrowdCoin</Menu.Item>
+      </Link>
+
       <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
-        <Menu.Item>+</Menu.Item>
+        <Link route="/">
+          <Menu.Item>Campaigns</Menu.Item>
+        </Link>
+        <Link route="/campaigns/new">
+          <Menu.Item>+</Menu.Item>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
