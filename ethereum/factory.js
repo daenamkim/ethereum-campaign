50217c9db011d7072708086a86ca98855290174c
Daenam Kim <daenam.kim@gmail.com>
2019-04-09 14:10:12 +0900

    Add summry functions in contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x6E6CBF609f93bd8132fDcf4DDc10674FA1C8e6dd'
+  '0xD21C1cC1887e34b1e857930969036e13Fb314A86'
 );
 
 export default instance;
