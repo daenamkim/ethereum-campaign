45ab904c44264c68504005996c54aaa74b9e9214
Daenam Kim <daenam.kim@gmail.com>
2019-04-29 09:36:47 +0900

    Change some server settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.prepare().then(() => {
   createServer(handler).listen(port, err => {
